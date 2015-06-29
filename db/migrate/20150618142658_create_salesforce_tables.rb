c578c51f7623e698649765b6b581b0138f373c51
Pedro Belo <pedro@heroku.com>
2015-06-29 16:49:39 -0700

    add contact table

--- db/migrate/20150618142658_create_salesforce_tables.rb
+++ db/migrate/20150618142658_create_salesforce_tables.rb
@@ -2,6 +2,19 @@ class CreateSalesforceTables < ActiveRecord::Migration
   def up
     execute "CREATE SCHEMA salesforce"
     change_schema("salesforce") do
+      create_table :contact do |t|
+        t.column :mailingstreet, "varchar(255)"
+        t.column :mailingcity, "varchar(40)"
+        t.column :mailingstate, "varchar(80)"
+        t.column :mailingcountry, "varchar(80)"
+        t.column :mailingpostalcode, "varchar(20)"
+        t.column :firstname, "varchar(40)"
+        t.column :lastname, "varchar(80)"
+        t.column :phone, "varchar(40)"
+        t.column :sfid, "varchar(18)"
+        t.column :spree_id__c, "varchar(14)"
+      end
+
       create_table :orders__c do |t|
         t.text :customer_email__c
       end
