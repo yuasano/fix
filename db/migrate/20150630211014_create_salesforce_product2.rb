76f18c8efe49e1130bfdc4dc320c67ab37aeedc4
Pedro Belo <pedro@heroku.com>
2015-07-01 17:21:39 -0700

    remove debug

--- db/migrate/20150630211014_create_salesforce_product2.rb
+++ db/migrate/20150630211014_create_salesforce_product2.rb
@@ -2,7 +2,6 @@ class CreateSalesforceProduct2 < ActiveRecord::Migration
   def up
     return if ENV["DEPLOYMENT"] == "production"
     HerokuConnect.change_schema("salesforce") do
-      puts ActiveRecord::Base.connection.schema_search_path
       create_table :product2 do |t|
         t.column :productcode, "varchar(255)"
         t.column :name, "varchar(255)"
