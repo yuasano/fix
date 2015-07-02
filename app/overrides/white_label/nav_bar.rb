00d3e02823f5125025a971c6a44826c55857ca54
Mars Hall <mars@heroku.com>
2015-07-01 17:22:05 -0700

    More white labelling.

---
+++ app/overrides/white_label/nav_bar.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
+                     :replace      => "#top-nav-bar",
+                     :name         => "top_nav_bar",
+                     :partial      => "white_label/shared/top_nav_bar")
