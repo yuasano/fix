75182dac3757e02ad2696689aac1ac73d6e4dcec
Mars Hall <mars@heroku.com>
2015-07-17 15:20:49 -0700

    layout Taxon (Kit) screen

--- app/overrides/white_label/taxon_hero_image.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/taxons/show",
-                     :insert_top   => "[data-hook='taxon_products']",
-                     :name         => "taxon_hero_image",
-                     :template     => "white_label/taxons/_hero_image")
--- app/overrides/white_label/taxon_sidebar_navigation.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/taxons/show",
-                     :replace      => "[data-hook='taxon_sidebar_navigation']",
-                     :name         => "taxon_sidebar_navigation",
-                     :template     => "white_label/taxons/_sidebar_navigation")
