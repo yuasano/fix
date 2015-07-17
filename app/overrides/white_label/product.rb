676db7b37f101a1f6d5cf5c0065f36a7e4661e98
Mars Hall <mars@heroku.com>
2015-07-17 15:38:48 -0700

    reveal Taxon (Kit) context on Product screen

--- app/overrides/white_label/product.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :replace      => "[data-hook='product_show']",
-                     :name         => "product",
-                     :template     => "white_label/products/show")
