6aea1609925b2f366651e1f8f81cff1e8f79de97
Mars Hall <mars@heroku.com>
2015-07-21 11:41:42 -0700

    Add new HTML head Deface hash

--- app/overrides/white_label/head.rb
+++ app/overrides/white_label/head.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
                      :insert_bottom => "[data-hook='inside_head']",
                      :name          => "spree_application_head",
-                     :partial       => "white_label/shared/head")
+                     :partial       => "white_label/shared/head",
+                     :original      => '3238a36183f493276bda180ca7b3430fb0cde007')
