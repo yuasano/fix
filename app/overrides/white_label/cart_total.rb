7ba4fd0d525cd89f6dd24f4ad5dec18d90a743fb
Mars Hall <mars@heroku.com>
2015-08-03 16:42:43 -0700

    Add original hashes for recently added Deface overrides.

--- app/overrides/white_label/cart_total.rb
+++ app/overrides/white_label/cart_total.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/orders/_form",
                      :replace      => "tr.cart-total",
                      :name         => "cart_total",
-                     :partial      => "white_label/orders/cart_total")
+                     :partial      => "white_label/orders/cart_total",
+                     :original     => '0d4ca2991462fc5cd24fb8f439a1c8de7758c925')
