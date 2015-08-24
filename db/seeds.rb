226e258e3a01fd623928d5d73186d7b510698e55
Pedro Belo <pedro@heroku.com>
2015-08-24 15:43:49 -0700

    remove debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,9 @@ Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 # for local development we need a standard Salesforce Pricebook set
 if ENV["DEPLOYMENT"] != "production"
-  puts ActiveRecord::Base.connection.execute("
+  ActiveRecord::Base.connection.execute("
     INSERT INTO salesforce.pricebook2 (sfid, name, isstandard)
     VALUES ('A001', 'Standard', TRUE)
   ")
+  puts "Created standard Salesforce pricebook."
 end
