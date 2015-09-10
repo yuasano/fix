a5efc24b96d34185ed6d032d26c974c76ccb4d33
Pedro Belo <pedro@heroku.com>
2015-09-10 13:15:18 -0700

    use connection pool with memcache
    should work better with puma in threaded mode (otherwise puma
    threads will compete to fetch the memcache instance from rails)

--- Gemfile
+++ Gemfile
@@ -51,9 +51,8 @@ group :development, :test do
   gem 'spring'
 end
 
-
+gem 'connection_pool'
 gem 'spree', '3.0.4'
 gem 'spree_gateway', github: 'spree/spree_gateway', branch: '3-0-stable'
 gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '3-0-stable'
-
 gem 'dalli'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,9 @@ Rails.application.configure do
       :failover => true,
       :socket_timeout => 1.5,
       :socket_failure_delay => 0.2,
-      :namespace => release_commit }
+      :namespace => release_commit,
+      :pool_size => Integer(ENV['MAX_THREADS'] || 5)
+    }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
